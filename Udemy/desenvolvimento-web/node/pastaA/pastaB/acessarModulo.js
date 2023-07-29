1ad40ad032d2bd3ba8acd12e876105dfc0fdea31
nickolss <nickolasmaiaraujo@gmail.com>
2023-07-28 21:23:57 -0300

    Adicionado aula do dia 28/07/23 de JS do curso da Udemy

---
+++ Udemy/desenvolvimento-web/node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,14 @@
+const moduloA = require("../../moduloA");
+console.log(moduloA.ola);
+
+const saudacao = require("saudacao");
+console.log(saudacao.ola);
+
+const pastaC = require("./pastaC")
+console.log(pastaC.ola2)
+
+// const http = require("http");
+// http.createServer((request, resposta) => {
+// 	resposta.write("Bom dia");
+// 	resposta.end();
+// }).listen(8080);

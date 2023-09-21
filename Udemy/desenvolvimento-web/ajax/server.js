e0207a278e39f3b5029bd74acb8f2bc56dd259ae
nickolss <nickolasmaiaraujo@gmail.com>
2023-09-21 14:59:43 -0300

    Adicionada aula 21-09-23 do curso de web moderno da Udemy

--- Udemy/desenvolvimento-web/ajax/server.js
+++ Udemy/desenvolvimento-web/ajax/server.js
@@ -1,13 +1,33 @@
-import express from 'express';
-import bodyparser from 'body-parser';
+import express from "express";
+import bodyparser from "body-parser";
+import multer from "multer";
 
 const app = express();
-app.use(express.static("."));
+app.use(express.static(".")); //Funciona como o live server, exibindo os arquivos estáticos
 app.use(bodyparser.urlencoded({ extended: true })); //Qualquer requisição que chegar será transformada para ter um "corpo"
 app.use(bodyparser.json());
 
-app.get("/teste", (req, resp, next) => {
-	resp.send("OK");
+const storage = multer.diskStorage({
+	destination: (req, file, callback) => {
+		callback(null, "./upload");
+	},
+	filename: (req, file, callback) => {
+		callback(null, `${Date.now()}_${file.originalname}`);
+	},
+});
+
+const upload = multer({
+	storage,
+}).single("arquivo");
+
+app.post("/upload", (req, resp, next) => {
+	upload(req, resp, (erro) => {
+		if (erro) {
+			return resp.send("Ocorreu um erro");
+		}
+
+		resp.end("Concluido com Sucesso");
+	});
 });
 
 app.listen(8081, () => console.log("Executando Server"));

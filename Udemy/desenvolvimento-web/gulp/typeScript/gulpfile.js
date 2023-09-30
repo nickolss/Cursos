b6deddac361b31029492674c5307b66289b39d4c
nickolss <nickolasmaiaraujo@gmail.com>
2023-09-30 19:18:27 -0300

    Adicionada aula 30-09-23 do curso de web moderno da Udemy

---
+++ Udemy/desenvolvimento-web/gulp/typeScript/gulpfile.js
@@ -0,0 +1,11 @@
+const { series } = require('gulp');
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+
+function transformarTs(cb) {
+	return tsProject.src().pipe(tsProject()).pipe(gulp.dest('build'));
+}
+
+exports.default = series(transformarTs);
+    

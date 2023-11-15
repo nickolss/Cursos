6247d8d9e5fbacac5ed27fc5a6d00b6e0379ea01
nickolss <nickolasmaiaraujo@gmail.com>
2023-11-15 20:51:57 +0000

    Adicionada aula 1 e 1.1 de C do Canal Pietro Martins de Oliveira

---
+++ Youtube/C/aula1.1/helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  printf("Hello World!\n"); //O \n é um símbolo para quebrar linha
+  
+  //system("pause");  No Windows, esse comando serve para ficar esperando o usuário digitar alguma coisa para fechar o programa. 
+}
